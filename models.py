5b8349b3945574f856ea3850ec83dc05ce41910d
Carter LaSalle <carterlasalle@gmail.com>
2024-10-13 17:40:59 -0700

    Update .gitignore

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))  # Increased from 128 to 256
 
     def __repr__(self):
         return f'<User {self.username}>'
